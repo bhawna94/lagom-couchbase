8218affcd8250841c309441db98df263c2735a6b
bhawna <bhawna@knoldus.in>
2018-07-12 05:16:57 +0530

    observable implementation

--- project-impl/src/main/java/edu/knoldus/project/impl/EmployeeServiceImpl.java
+++ project-impl/src/main/java/edu/knoldus/project/impl/EmployeeServiceImpl.java
@@ -14,10 +14,15 @@ import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import edu.knoldus.project.api.Employee;
 import edu.knoldus.project.api.EmployeeService;
-import javafx.beans.Observable;
+import rx.Observable;
 import rx.Observer;
+import rx.functions.Action1;
+import rx.observables.SyncOnSubscribe;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 
 
@@ -25,6 +30,38 @@ public class EmployeeServiceImpl implements EmployeeService {
     private Cluster cluster;
     private Bucket bucket;
     private ObjectMapper mapper = new ObjectMapper();
+    Observable<Integer> observable = Observable.just(1, 2, 3, 4,5,6,7,8,9,0);
+
+
+    /**
+     * create method in Observable
+     */
+    Observable<Integer> getObservable = Observable.create(SyncOnSubscribe.createStateless(new Action1<Observer<? super Integer>>() {
+        @Override
+        public void call(Observer<? super Integer> observer) {
+            for (int i = 2; i < 9; i++) {
+                if(i < 8) {
+                    System.out.println("value created");
+                    observer.onNext(3);
+                }
+                else
+                {
+                    observer.onCompleted();
+                }
+            }
+
+
+//            Random rand = new Random();
+//            observer.onNext(rand.nextInt(10));
+            /*System.out.println("value created");
+            observer.onNext(1);
+            observer.onCompleted();*/
+
+        }
+    }));
+
+    Observable<Integer> abc = Observable.just(1,2,3,4,5,6);
+
 
     public EmployeeServiceImpl() {
         cluster = couchbaseConnector();
@@ -69,6 +106,7 @@ public class EmployeeServiceImpl implements EmployeeService {
             JsonObject empData = JsonObject.fromJson(jsonNode.toString());
             bucket.upsert(JsonDocument.create(empData.get("ename").toString(), empData));
 
+
             return CompletableFuture.completedFuture("Done");
 
 
@@ -81,12 +119,26 @@ public class EmployeeServiceImpl implements EmployeeService {
             Employee emp = null;
             JsonObject jsonObject = bucket.get(documentId).content();
             String jsonString = jsonObject.toString();
+            System.out.println(observable);//observable of Integer
+            observable.subscribe(value -> System.out.println("Value" + value));
+            observable.map(a -> a * 10).subscribe(a -> System.out.println(a));
+            observable.first().subscribe(a -> System.out.println("first Value" + a));
+            System.out.println("................");
+            observable.skip(2).subscribe(a -> System.out.println("value" + a));
+            List<Integer> list = Arrays.asList(1, 2, 3);
+            System.out.println("................");
+            Observable<Integer> observable1 = Observable.from(list);
+            observable1.subscribe(a -> System.out.println(a));
+            System.out.println(getObservable);
+            getObservable.subscribe(val -> System.out.println(val));
+
             try {
                 emp = mapper.readValue(jsonString, Employee.class);
             } catch (IOException ex) {
                 ex.getMessage();
             }
             return CompletableFuture.completedFuture(emp);
+
 
         };
     }
---
+++ project-impl/src/main/java/edu/knoldus/project/impl/Operation.java
@@ -0,0 +1,4 @@
+package edu.knoldus.project.impl;
+
+public class Operation {
+}
